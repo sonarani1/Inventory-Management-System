605cbf5c7ed5b195b0423909e6d7ccdfec033039
sonarani1 <sonrich066@gmail.com>
2025-11-08 15:30:13 +0530

    Adding api.jsx in sevices

---
+++ frontend/inventory_vite/src/services/api.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import ErrorHandler from '../utils/errorHandler';
+
+const API = axios.create({
+  baseURL: 'http://localhost:8000/api/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Attach the token if available
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem('access');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => Promise.reject(error));
+
+// Response interceptor for global error handling
+API.interceptors.response.use(
+  response => response,
+  error => {
+    ErrorHandler.handle(error, 'API Request');
+    return Promise.reject(error);
+  }
+);
+
+export default API;
